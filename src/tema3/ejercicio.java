869b2daa774df48b6531020f0a33c432e8440945
Josemi <fs.josemiguel@gmail.com>
2022-08-24 13:53:13 +0200

    Ejercicio Tema 3

---
+++ src/tema3/ejercicio.java
@@ -0,0 +1,34 @@
+package tema3;
+
+public class ejercicio {
+    public static void main(String[] args) {
+        // Firsts part
+        int result;
+        result = suma(1, 2, 3);
+        System.out.println("Resultado de la suma: " + result);
+
+        // Second part
+        Car car1 = new Car();
+        car1.plusDoor();
+        System.out.println("Número de puertas" + car1.door);
+    }
+
+    public static int suma(int num1, int num2, int num3) {
+        return (num1 + num2 + num3);
+
+    }
+
+    /**
+     * Class Car, implementa método para ampliar puertas.
+     */
+    public static class Car {
+        private int door = 0;
+
+        public Car() {
+        }
+
+        public void plusDoor() {
+            this.door++;
+        }
+    }
+}
